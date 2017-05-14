ccb91216f34144d42151086f258ec23abee4353b
topik mujianto <topikmujianto@topiks-macbook-air.local>
2017-05-15 04:45:54 +0700

    Initial Commit

---
+++ Kolaga/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kolaga
+//
+//  Created by topik mujianto on 5/15/17.
+//  Copyright © 2017 topik mujianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
